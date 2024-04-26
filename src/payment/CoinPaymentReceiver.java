5ec9727296fbed1d2d945c856694c01d781ba04c
Bekboldueva Aisuluu <bekbolduevaaisuluu06@gmail.com>
2024-04-26 15:50:14 +0600

    create the basic logic for accepting payment through coins.

---
+++ src/payment/CoinPaymentReceiver.java
@@ -0,0 +1,21 @@
+package payment;
+
+import java.util.Scanner;
+
+public class CoinPaymentReceiver extends PaymentReceiver {
+    private int amount;
+
+    @Override
+    public void acceptPayment(int amount) {
+        System.out.println("Бросьте монеты в монетоприемник.");
+        System.out.print("Введите количество монет: ");
+        amount = new Scanner(System.in).nextInt();
+        int productPrice = 100;
+        if (amount >= productPrice) {
+            System.out.println("Платеж через монетоприемник принят.");
+        } else {
+            System.out.println("Недостаточно монет для оплаты товара.");
+        }
+    }
+}
+
